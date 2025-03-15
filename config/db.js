dd7383df89273b83b8588c5dccb0f944a580de01
venugopalbalaji3 <154801616+venugopalbalaji3@users.noreply.github.com>
2025-03-15 16:57:36 +0530

    Add files via upload
    Added db connectivity url

---
+++ config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/task-management', // MongoDB connection string
+    // mongoOptions: {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    // },
+  };
+  
